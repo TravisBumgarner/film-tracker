cbb93baf4096fc7c08878c28a86d1d403fd941e8
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-06-18 22:31:11 -0600

    added first component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
-import { Text } from 'react-native-paper'
+import Typography from '@/shared/components/Typography'
+import { SafeAreaView } from 'react-native'
 
 const Rolls = () => {
-  return <Text>Rolls</Text>
+  return (
+    <SafeAreaView>
+      <Typography variant="h1">Rolls</Typography>
+    </SafeAreaView>
+  )
 }
 
 export default Rolls
