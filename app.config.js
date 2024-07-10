67ea9bebdf0e7cf7c3c8756c7e9c5a819d2b59d1
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-07-10 17:56:49 -0600

    added updated build steps

---
+++ app.config.js
@@ -0,0 +1,47 @@
+import 'dotenv/config'
+
+const config = () => ({
+  expo: {
+    name: process.env.APP_NAME || 'Film',
+    slug: 'ideas-down-quickly',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'myapp',
+    userInterfaceStyle: 'automatic',
+    splash: {
+      image: './assets/images/splash.png',
+      resizeMode: 'cover',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      bundleIdentifier: process.env.EXPO_BUNDLE_IDENTIFIER || 'com.sillysideprojects.film.prod',
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: ['expo-router', 'expo-font'],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: '5a7a8c6a-cd24-4ec0-8504-37c853285037',
+      },
+    },
+  },
+})
+
+export default config()
