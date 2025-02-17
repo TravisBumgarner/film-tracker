3f7fdc6043707802151db8aa4c65c27482a50930
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-02-17 00:20:56 -0600

    working in internal

--- db/migrations/migrations.js
+++ db/migrations/migrations.js
@@ -1,20 +1,11 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
-import journal from './meta/_journal.json';
-import m0000 from './0000_luxuriant_microchip.sql';
-import m0001 from './0001_wandering_gorilla_man.sql';
-import m0002 from './0002_long_weapon_omega.sql';
-import m0003 from './0003_breezy_franklin_richards.sql';
-import m0004 from './0004_nervous_drax.sql';
+import m0000 from './0000_magical_stephen_strange.sql'
+import journal from './meta/_journal.json'
 
-  export default {
-    journal,
-    migrations: {
-      m0000,
-m0001,
-m0002,
-m0003,
-m0004
-    }
-  }
-  
+export default {
+  journal,
+  migrations: {
+    m0000,
+  },
+}
